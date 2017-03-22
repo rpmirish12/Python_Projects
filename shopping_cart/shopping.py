a873cb1a4cb528d5c2ff47c8e616a44d87f3955e
Ryan Miller <rmille14@alumni.nd.edu>
2017-03-22 10:50:41 -0400

    New Folder structure

---
+++ shopping_cart/shopping.py
@@ -0,0 +1,21 @@
+
+
+def get_item():
+    print("Please enter an item: ")
+    return input()
+
+
+def go_shopping():
+    cart = []
+
+    while True:
+        item = get_item()
+        if item == "":
+            break
+        cart.append(item)
+
+    print(cart)
+    print("Finished!")
+
+
+go_shopping()
