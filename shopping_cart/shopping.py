0e5e8253d3c03517bedb5f33777c0959577ff0e1
Ryan Miller <rmille14@alumni.nd.edu>
2017-03-24 11:17:45 -0400

    New Folder structure

--- shopping_cart/shopping.py
+++ shopping_cart/shopping.py
@@ -5,16 +5,41 @@ def get_order():
     line = input()
     command = line[:1]
     item = line[2:]
+    return command, item
+
+
+def add_to_cart(item, cart):
+    if item not in cart:
+        cart[item] = 1
+    elif item in cart:
+        cart[item] += 1
+
+
+def delete_from_cart(item, cart):
+    if cart[item] > 1:
+        cart[item] -= 1
+    elif cart[item] == 1:
+        del cart[item]
+
+
+def process_order(order, cart):
+    command, item = order
+    if command == "a":
+        add_to_cart(item, cart)
+    elif command == "d" and item in cart:
+        delete_from_cart(item, cart)
+    elif command == "q":
+        return False
+    return True
 
 
 def go_shopping():
-    cart = []
+    cart = dict()
 
     while True:
-        item = get_order()
-        if item == "":
+        order = get_order()
+        if not process_order(order, cart):
             break
-        cart.append(item)
 
     print(cart)
     print("Finished!")
