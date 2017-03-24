e216d1e782cf7badb4c2ccee5d791a9d2c73d71f
Ryan Miller <rmille14@alumni.nd.edu>
2017-03-24 13:40:49 -0400

    New Folder structure

--- shopping_cart/shoppingclass.py
+++ shopping_cart/shoppingclass.py
@@ -1,49 +1,12 @@
-class Cart:
+import sales
 
-    def __init__(self):
-        self._contents = dict()
-
-    def __repr__(self):
-        return "{0} {1}".format(Cart, self.__dict__)
-
-    def process(self, u_order):
-        if u_order.add:
-            if u_order.item not in self._contents:
-                self._contents[u_order.item] = 0
-            self._contents[u_order.item] += 1
-        elif u_order.delete:
-            self._contents[u_order.item] -= 1
-            if self._contents[u_order.item] <= 0:
-                del self._contents[u_order.item]
-
-
-class Order:
-
-    def __init__(self):
-        self.quit = False
-        self.add = False
-        self.delete = False
-        self.item = None
-
-    def get_input(self):
-        print("[command] [item] (command is a to add, d to delete, q to quit)")
-        line = input()
-        command = line[:1]
-        self.item = line[2:]
-        if command == "q":
-            self.quit = True
-        elif command == "a":
-            self.add = True
-        elif command == "d":
-            self.delete = True
-
-cart = Cart()
-order = Order()
+cart = sales.Cart()
+order = sales.Order()
 order.get_input()
 
 while not order.quit:
     cart.process(order)
-    order = Order()
+    order = sales.Order()
     order.get_input()
 
 print(cart)
